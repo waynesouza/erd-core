429e208d7db1b67fbc233cd5348ee9b900f46127
waynesouza <waynesouza07@gmail.com>
2024-02-13 21:33:32 -0300

    feat: added configs

--- src/main/java/com/erd/core/controller/WebSocketController.java
+++ src/main/java/com/erd/core/controller/WebSocketController.java
@@ -3,12 +3,16 @@ package com.erd.core.controller;
 import com.erd.core.dto.request.DiagramDataRequestDTO;
 import com.erd.core.dto.response.DiagramDataResponseDTO;
 import com.erd.core.service.WebSocketService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class WebSocketController {
+
+    private static final Logger logger = LoggerFactory.getLogger(WebSocketController.class);
 
     private final WebSocketService webSocketService;
     private final SimpMessagingTemplate messagingTemplate;
@@ -20,6 +24,7 @@ public class WebSocketController {
 
     @MessageMapping("/send")
     public void handle(DiagramDataRequestDTO diagramDataRequestDto) {
+        logger.info("Received message: {}", diagramDataRequestDto);
         DiagramDataResponseDTO savedDto = webSocketService.save(diagramDataRequestDto);
         messagingTemplate.convertAndSend("/topic/receive", savedDto);
     }
--- src/main/java/com/erd/core/dto/NodeDataDTO.java
+++ src/main/java/com/erd/core/dto/NodeDataDTO.java
@@ -1,10 +1,15 @@
 package com.erd.core.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.util.List;
 
 public class NodeDataDTO {
 
+    @JsonProperty
     private String key;
+
+    @JsonProperty
     private List<ItemDTO> items;
 
 }

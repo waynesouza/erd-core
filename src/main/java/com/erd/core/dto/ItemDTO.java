033fc011875136c822020af36c5640f8a5105704
waynesouza <waynesouza07@gmail.com>
2024-03-27 20:40:22 -0300

    feat: added new fields in DTO diagram

--- src/main/java/com/erd/core/dto/ItemDTO.java
+++ src/main/java/com/erd/core/dto/ItemDTO.java
@@ -10,6 +10,21 @@ public class ItemDTO {
     @JsonProperty
     private String type;
 
+    @JsonProperty
+    private Boolean pk;
+
+    @JsonProperty
+    private Boolean unique;
+
+    @JsonProperty
+    private String defaultValue;
+
+    @JsonProperty
+    private Boolean nullable;
+
+    @JsonProperty
+    private Boolean autoIncrement;
+
     public String getName() {
         return name;
     }
@@ -25,4 +40,45 @@ public class ItemDTO {
     public void setType(String type) {
         this.type = type;
     }
+
+    public Boolean getPk() {
+        return pk;
+    }
+
+    public void setPk(Boolean pk) {
+        this.pk = pk;
+    }
+
+    public Boolean getUnique() {
+        return unique;
+    }
+
+    public void setUnique(Boolean unique) {
+        this.unique = unique;
+    }
+
+    public String getDefaultValue() {
+        return defaultValue;
+    }
+
+    public void setDefaultValue(String defaultValue) {
+        this.defaultValue = defaultValue;
+    }
+
+    public Boolean getNullable() {
+        return nullable;
+    }
+
+    public void setNullable(Boolean nullable) {
+        this.nullable = nullable;
+    }
+
+    public Boolean getAutoIncrement() {
+        return autoIncrement;
+    }
+
+    public void setAutoIncrement(Boolean autoIncrement) {
+        this.autoIncrement = autoIncrement;
+    }
+
 }
--- src/main/java/com/erd/core/dto/NodeDataDTO.java
+++ src/main/java/com/erd/core/dto/NodeDataDTO.java
@@ -3,8 +3,12 @@ package com.erd.core.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.List;
+import java.util.UUID;
 
 public class NodeDataDTO {
+
+    @JsonProperty
+    private UUID id;
 
     @JsonProperty
     private String key;
